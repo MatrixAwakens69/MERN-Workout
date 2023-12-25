2159f2c452687396676e1bc092c315aa586e74f9
Ponnanna-AH <siliconguy878@gmail.com>
2023-12-25 18:36:16 +0530

    Initial Commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {
+    getWorkouts,
+    getSingleWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController');
+
+const router = express.Router();
+
+router.get('/', getWorkouts);
+
+router.get('/:id', getSingleWorkout);
+
+router.post('/', createWorkout);
+
+router.delete('/:id', deleteWorkout);
+
+router.patch('/:id', updateWorkout);
+
+module.exports = router;
